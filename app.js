107f067fb82fb17346e8d754dc4939a3574be52f
AliScripter <araz.hello@gmail.com>
2024-10-01 01:05:09 +0330

    until 01H : 05m

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('./music/food.mp3');
 const gameOverSound = new Audio('./music/gameover.mp3');
 const moveSound = new Audio('./music/move.mp3');
 const musicSound = new Audio('./music/music.mp3');
-let speed = 2;
+let speed = 5;
 let score = 0;
 let lastPaintTime = 0;
 let sankeArr = [{ x: 13, y: 15 }];
@@ -23,8 +23,23 @@ function main(ctime) {
   gameEngine();
 }
 
-function isCollide(sarr) {
-  return false;
+function isCollide(snake) {
+  // If you bump into yourself
+
+  for (let i = 1; i < sankeArr.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      return true;
+    }
+
+    // If you bump into the wall
+    if (
+      snake[0].x >= 18 ||
+      (snake[0].x <= 0 && snake[0].y >= 18) ||
+      snake[0].y <= 0 
+    ) {
+      return true;
+    }
+  }
 }
 
 function gameEngine() {
@@ -41,10 +56,28 @@ function gameEngine() {
 
   // if you have eaten the food , increment the score and regenerate the food
 
-  if (sankeArr[0].y === food.y && sankeArr[0].x === food.x) 
-    sankeArr.unshift({x : sankeArr[0].x + inputDir.x , y : sankeArr[0].y + inputDir.y})
+  if (sankeArr[0].y === food.y && sankeArr[0].x === food.x) {
+    foodSound.play();
+    sankeArr.unshift({
+      x: sankeArr[0].x + inputDir.x,
+      y: sankeArr[0].y + inputDir.y,
+    });
+
+    let a = 2;
+    let b = 16;
+    food = {
+      x: 2 + Math.round(a + (b - a) * Math.random()),
+      y: 2 + Math.round(a + (b - a) * Math.random()),
+    };
   }
 
+  // Moving the snake
+  for (let i = sankeArr.length - 2; i >= 0; i--) {
+    sankeArr[i + 1] = { ...sankeArr[i] };
+  }
+
+  sankeArr[0].x += inputDir.x;
+  sankeArr[0].y += inputDir.y;
 
   // Part 2 : Display the snake and food
   // Display the snake
